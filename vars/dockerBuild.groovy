8cec4abdc30a07d785dd9d6aea44913395c4a9b7
ali <aligsr@yahoo.com>
2023-11-02 22:12:09 -0400

    edit

--- vars/dockerBuild.groovy
+++ vars/dockerBuild.groovy
@@ -1,6 +1,6 @@
 def call(String project, String ImageTag, String DockerHubUser){
     sh """
-      docker image build -t ${DockerHubUser}/${project} . 
+      docker image build -t ${alias64}/${project} . 
       docker image tag ${DockerHubUser}/${project} ${DockerHubUser}/${project}:${ImageTag}
       docker image tag ${DockerHubUser}/${project} ${DockerHubUser}/${project}:latest
      """
